d2512e9058b8cdb592f661abd7226798f513cffc
ardiapri <ardiapri882@gmail.com>
2024-05-14 17:34:39 +0700

    void printall

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -10,6 +10,15 @@ public:
 
     void setID();
 
-   
+    void printALL();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+
+    }
+};
+
+int mahasiswa::nim = 113;
 
     
